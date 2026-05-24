a80ef996883116eb7a3d8f0d1e3f5a0320162dd3
Shruti Verma <shrutiverma10@gmail.com>
2026-05-24 10:16:20 +0530

    fixed bug

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ dotenv.config();  // ← must be first
 connectDB();      // ← then connect DB
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: ['http://localhost:5173','https://donezo-sepia.vercel.app'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth',authRoutes);
