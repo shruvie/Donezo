5b7a902e6d3423550d6d8c2af63f69249032e9a0
Shruti Verma <shrutiverma10@gmail.com>
2026-05-24 10:10:18 +0530

    abit

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
   res.send('API is running!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);  // ← callback function not a string
 });
